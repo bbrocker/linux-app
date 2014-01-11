b0159a0aa2caf9e426a2b58ac1fae9a64c9c5ec8
bbrocker <williambrocker@yahoo.com>
2014-01-11 15:02:10 -0500

    Initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LinuxApp::Application.config.secret_key_base = '19d9fc6a086d869fd19c8daed0a58c5a2308dadd648b667e61dbe404b9ebe4cbfdf0790c0d029be5a9a7e31f5af86c66dac2c03e39dfe7e43b0fbf7bc5bce8f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LinuxApp::Application.config.session_store :cookie_store, key: '_linux_app_session'
